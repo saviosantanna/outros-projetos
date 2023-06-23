855fb463b7ea399951d353f4ef27c33d894ff6f3
Sávio Sant'anna <saviosantannadesa@gmail.com>
2023-06-23 17:30:15 -0300

    inclusao botao excluir, e iniciado implementação botao editar

--- RegistradorConsultas/script.js
+++ RegistradorConsultas/script.js
@@ -1,29 +1,41 @@
 var consultas = []
 
-
+/* CAPTURAR ELEMENTOS DO HTML */
 
 var nomeForm = window.document.getElementById('formulario')
 nomeForm.addEventListener("submit", function (e) {
-
-    var date = new Date()
-    let ano = date.getFullYear();
-    let mes = date.getMonth() + 1;
-    let dia = date.getDate();
-    let hora = date.getHours();
-    let min = date.getMinutes();
-
-    horario = `${dia}/${mes}/${ano} às ${hora}h${min}`;
-
     texto = e.target[0].value;
-    e.preventDefault();
 
-    e.target[0].value = "";
+    if (texto == ".") {
+        alert("Favor preencher a consulta");
+    }
+    else {
+        var date = new Date()
+        let ano = date.getFullYear();
+        let mes = date.getMonth() + 1;
+        let dia = date.getDate();
+        let hora = date.getHours();
+        let min = date.getMinutes();
 
-    consulta = { texto, horario }
-    consultas = [...consultas, consulta]
+        horario = `${dia}/${mes}/${ano} às ${hora}h${min}`;
 
-    atualizarDados(consulta);
+
+        //console.log(texto)
+        e.preventDefault();
+
+        e.target[0].value = "";
+        
+        var edit = "";
+        consulta = { texto, horario, edit }
+        consultas = [...consultas, consulta]
+        
+        atualizarDados(consulta);
+        console.log(consulta)
+    }
+
 })
+
+/* ATUALIZAR LISTA DAS CONSULTAS */
 
 function atualizarDados(consulta) {
 
@@ -33,50 +45,46 @@ function atualizarDados(consulta) {
 
     for (i = 0; i < consultas.length; i++) {
         res.innerHTML += `<div class="consulta">
-    <span>${consultas[i].texto} - realizada às ${consultas[i].horario}</span>
+    <span>${consultas[i].texto} - realizada às ${consultas[i].horario}${consultas[i].edit}</span>
+    <div class="botoes">
+    <ion-icon name="pencil-outline" class="editar"></ion-icon>
     <ion-icon name="trash-bin-outline" class="lixeira"></ion-icon>
+    </div>
 </div>`
     }
 
-    console.log(consultas)
+    //console.log(consultas)
 
     var lixo = window.document.getElementsByClassName('lixeira')
     for (let i = 0; i < consultas.length; i++) {
-        lixo[i].addEventListener("click", function (teste) {
-            teste.target.style.color = 'yellow'
-            console.log(i);
+        lixo[i].addEventListener("click", function () {
             removerDado(i);
         })
     }
-}
 
-function removerDado(i){
-    console.log("remover indice "+i)
-    consultas.splice(i,1);
-    atualizarDados();
-    console.log(consultas);
-}
-
-
-
-/*
-
-
-    var excluir = window.document.getElementsByClassName('consulta')
-    var lixo = window.document.getElementsByClassName('lixeira')
-
-    for (let i = 0; i < lixo.length; i++) {
-        lixo[i].addEventListener("click", function (teste) {
-
-            //teste.target.style.color = "green";
-            console.log(teste.target)
-
-            excluir[i].remove();
-            console.log(lixo.length)
-            teste.target.removeEventListener()
-
+    var lapis = window.document.getElementsByClassName('editar')
+    for (let i = 0; i < consultas.length; i++) {
+        lapis[i].addEventListener("click", function () {
+            editarDado(i);
         })
-
     }
 
-*/
+}
+
+
+/* REMOVER CONSULTA */
+
+function removerDado(i) {
+    consultas.splice(i, 1);
+    atualizarDados();
+    //console.log(consultas);
+}
+
+/* EDITAR CONSULTA */
+function editarDado(i){
+    consultas[i].texto = "TEXTO EDITADO";
+    consultas[i].edit = " (editado)";
+    atualizarDados(consultas);
+}
+
+
